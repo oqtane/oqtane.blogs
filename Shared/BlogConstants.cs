a4f8f8d6755a493a1cb09bc8161390c66763e980
vnetonline <vbelani@amazingsolutions.com.au>
2024-08-29 13:15:24 +1000

    [ENH] - Added _rowCssClass parameter to Pager and  Settings

--- Shared/BlogConstants.cs
+++ Shared/BlogConstants.cs
@@ -7,6 +7,7 @@ namespace Oqtane.Blogs.Shared
         public const int DefaultVersion = 1;
 
         public const string DefaultColumnCssClass = "col-lg-4 col-md-6";
+        public const string DefaultRowCssClass = "row";
         public const string DefaultSummaryTemplate = "<a href=\"[URL]\"><h2>[TITLE]</h2></a><p>[SUMMARY]</p>";
         public const string DefaultDetailTemplate = "<h2>[TITLE]</h2><p>[CONTENT]</p>";
         public const string DefaultCommentTemplate = "<figure><blockquote class=\"blockquote\"><p>[COMMENT]</p></blockquote><figcaption class=\"blockquote-footer\">[NAME] on [DATE] at [TIME]</figcaption></figure>";
